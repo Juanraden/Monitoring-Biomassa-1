62802b6113765d45dd2ce4689880bd7b84fe3496
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-03 14:31:40 +0700

    setup fragment settings

--- app/src/main/java/com/kedaireka/monitoring_biomassa/ui/SettingsFragment.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/ui/SettingsFragment.kt
@@ -1,19 +1,61 @@
 package com.kedaireka.monitoring_biomassa.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.kedaireka.monitoring_biomassa.R
+import com.kedaireka.monitoring_biomassa.databinding.FragmentSettingsBinding
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), View.OnClickListener {
+    private lateinit var binding: FragmentSettingsBinding
+
+    private val _fragmentTag = "Setting_Fragment"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupOnClickListener()
+    }
+
+    private fun setupOnClickListener(){
+        with(binding){
+            aboutTv.setOnClickListener(this@SettingsFragment)
+            webTv.setOnClickListener(this@SettingsFragment)
+            sosmedTv.setOnClickListener(this@SettingsFragment)
+            feedbackTv.setOnClickListener(this@SettingsFragment)
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        p0.let {
+            when(it!!.id){
+                R.id.about_tv -> {
+                    Log.i(_fragmentTag, "About KJABB CLicked")
+                }
+                R.id.web_tv -> {
+                    Log.i(_fragmentTag, "Website Monitoring CLicked")
+                }
+                R.id.sosmed_tv -> {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/cemebsakedaireka/"))
+                    startActivity(intent)
+                }
+                else -> {
+                    Log.i(_fragmentTag, "none")
+                }
+            }
+        }
     }
 }
