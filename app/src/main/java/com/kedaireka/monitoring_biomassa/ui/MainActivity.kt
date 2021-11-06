610ab3805d7ab4e2bff6f5acea6f3458e6385473
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-06 11:06:48 +0700

    update main activity (no action bar)

--- app/src/main/java/com/kedaireka/monitoring_biomassa/ui/MainActivity.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/ui/MainActivity.kt
@@ -9,32 +9,15 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.kedaireka.monitoring_biomassa.R
 import com.kedaireka.monitoring_biomassa.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        setUpNavigation()
-    }
-
-    private fun setUpNavigation() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.addFragment, R.id.settingsFragment))
-
-        navController = navHostFragment.navController
-
-        NavigationUI.setupWithNavController(binding.bottomNavView, navController)
-
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
