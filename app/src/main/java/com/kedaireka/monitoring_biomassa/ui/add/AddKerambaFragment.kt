e56d6d32e8f9c63706bb71a6e7754c4e872726c3
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-12 23:11:46 +0700

    refacor datepickerfragment

--- app/src/main/java/com/kedaireka/monitoring_biomassa/ui/add/AddKerambaFragment.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/ui/add/AddKerambaFragment.kt
@@ -1,7 +1,6 @@
 package com.kedaireka.monitoring_biomassa.ui.add
 
 import android.app.DatePickerDialog
-import android.app.Dialog
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.LayoutInflater
@@ -9,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import android.widget.TextView
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
@@ -19,13 +17,14 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import com.kedaireka.monitoring_biomassa.R
 import com.kedaireka.monitoring_biomassa.databinding.FragmentAddKerambaBinding
+import com.kedaireka.monitoring_biomassa.ui.DatePickerFragment
 import com.kedaireka.monitoring_biomassa.util.convertLongToDateString
 import com.kedaireka.monitoring_biomassa.viewmodel.KerambaViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
 @AndroidEntryPoint
-class AddKerambaFragment : Fragment() {
+class AddKerambaFragment : Fragment(), DatePickerDialog.OnDateSetListener {
     private lateinit var binding: FragmentAddKerambaBinding
 
     private val kerambaViewModel by activityViewModels<KerambaViewModel>()
@@ -99,7 +98,7 @@ class AddKerambaFragment : Fragment() {
     }
 
     fun showDatePicker() {
-        DatePickerFragment.create().show(requireActivity().supportFragmentManager, "datePicker")
+        DatePickerFragment.create().show(childFragmentManager, "datePicker")
     }
 
     private fun isEntryValid(nama: String, ukuran: String): Boolean{
@@ -119,38 +118,9 @@ class AddKerambaFragment : Fragment() {
             navController.navigateUp(appBarConfiguration)
         }
     }
-}
-
-@AndroidEntryPoint
-class DatePickerFragment : DialogFragment(),
-    DatePickerDialog.OnDateSetListener {
-
-    private val kerambaViewModel by activityViewModels<KerambaViewModel>()
-
-    companion object {
-        fun create(): DatePickerFragment =
-            DatePickerFragment()
-
-        private var selectedYear: Int = 0
-        private var selectedMonth: Int = 0
-        private var selectedDay: Int = 0
-    }
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        return DatePickerDialog(requireContext(), this, year, month, day)
-    }
-
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        selectedYear = year
-        selectedMonth = month
-        selectedDay = dayOfMonth
         val selectedDate: Calendar = Calendar.getInstance()
-        selectedDate.set(selectedYear, selectedMonth, selectedDay)
+        selectedDate.set(year, month, dayOfMonth)
 
         kerambaViewModel.onSelectDateTime(selectedDate.timeInMillis)
     }
