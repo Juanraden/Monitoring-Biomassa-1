5ade21612d0b383c5085ffc4974063e4a3c3d66a
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-15 10:04:15 +0700

    add pakan fragment

---
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/ui/add/AddPakanFragment.kt
@@ -0,0 +1,51 @@
+package com.kedaireka.monitoring_biomassa.ui.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.kedaireka.monitoring_biomassa.R
+import com.kedaireka.monitoring_biomassa.databinding.FragmentAddPakanBinding
+
+class AddPakanFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddPakanBinding
+
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = FragmentAddPakanBinding.inflate(inflater, container, false)
+
+        navController = findNavController()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.addFragment,R.id.settingsFragment))
+
+        binding.toolbarFragment.setupWithNavController(navController, appBarConfiguration)
+
+        binding.toolbarFragment.setNavigationOnClickListener {
+            navController.navigateUp(appBarConfiguration)
+        }
+    }
+}
