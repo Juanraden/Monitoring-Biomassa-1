8bf19ce4afb50faf08e503d837935a9b2cb6ca84
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-24 21:58:21 +0700

    setup kerambaNetwork model

---
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/data/network/KerambaContainer.kt
@@ -0,0 +1,10 @@
+package com.kedaireka.monitoring_biomassa.data.network
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class KerambaContainer(
+    override val status: String,
+    override val message: String,
+    override val data: List<KerambaNetwork>,
+): BaseContainer<KerambaNetwork>()
