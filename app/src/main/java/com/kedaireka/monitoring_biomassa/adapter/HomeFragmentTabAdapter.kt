27782c05131f6b987881a1463301c3261b653d07
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-25 13:11:30 +0700

    change second tab to AddPakanFragment.kt

--- app/src/main/java/com/kedaireka/monitoring_biomassa/adapter/HomeFragmentTabAdapter.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/adapter/HomeFragmentTabAdapter.kt
@@ -3,7 +3,7 @@ package com.kedaireka.monitoring_biomassa.adapter
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.kedaireka.monitoring_biomassa.ui.KerambaFragment
-import com.kedaireka.monitoring_biomassa.ui.summary.PakanFragment
+import com.kedaireka.monitoring_biomassa.ui.add.AddPakanFragment
 
 class HomeFragmentTabAdapter constructor(fragment: Fragment): FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int =  2
@@ -11,7 +11,7 @@ class HomeFragmentTabAdapter constructor(fragment: Fragment): FragmentStateAdapt
     override fun createFragment(position: Int): Fragment {
         return when (position){
             0 -> KerambaFragment()
-            else -> PakanFragment()
+            else -> AddPakanFragment()
         }
     }
 }
