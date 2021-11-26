ba7676f361b318ae0182405cb602e10672c1d773
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-26 22:08:36 +0700

    update kerambaNetworkMapper

--- app/src/main/java/com/kedaireka/monitoring_biomassa/repository/KerambaRepository.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/repository/KerambaRepository.kt
@@ -1,6 +1,6 @@
 package com.kedaireka.monitoring_biomassa.repository
 
-import android.util.Log
+import android.annotation.SuppressLint
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.asLiveData
@@ -13,28 +13,41 @@ import com.kedaireka.monitoring_biomassa.service.MonitoringService
 import com.kedaireka.monitoring_biomassa.util.EntityMapper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.Response
+import java.text.SimpleDateFormat
 import javax.inject.Inject
 
 class KerambaRepository @Inject constructor(
     private val kerambaDAO: KerambaDAO,
     private val monitoringService: MonitoringService,
-    private val kerambaMapper: EntityMapper<Keramba, KerambaDomain>,
-    private val kerambaNetworkMapper: EntityMapper<KerambaNetwork, Keramba>
+    private val kerambaMapper: EntityMapper<Keramba, KerambaDomain>
 ) {
     val kerambaList: LiveData<List<KerambaDomain>> = Transformations.map(kerambaDAO.getAll().asLiveData()) { list ->
         list.map { kerambaMapper.mapFromEntity(it) }
     }
 
+    @SuppressLint("SimpleDateFormat")
     suspend fun refreshKeramba() {
         withContext(Dispatchers.IO){
-            try{
-                val kerambaContainer: KerambaContainer = monitoringService.getKerambaListAsync().await()
 
-                val kerambaList: List<Keramba> = kerambaContainer.data.map { kerambaNetworkMapper.mapFromEntity(it) }.toList()
+            val response: Response<KerambaContainer> = monitoringService.getKerambaListAsync().await()
 
-                kerambaDAO.insertAll(kerambaList)
-            } catch (e: Exception){
-                Log.e("REFRESH_KERAMBA", "refreshKeramba() error = ${e.message}")
+            if (response.code() == 200) {
+
+                val listKerambaNetwork: List<KerambaNetwork> = response.body()!!.data
+
+                val listKeramba: List<Keramba> = listKerambaNetwork.map { target ->
+                    Keramba(
+                        keramba_id = target.keramba_id.toInt(),
+                        nama_keramba = target.nama,
+                        ukuran = target.ukuran.toDouble(),
+                        tanggal_install = SimpleDateFormat("yyyy-MM-dd").parse(target.tanggal_install)!!.time
+                    )
+                }.toList()
+
+                kerambaDAO.insertAll(listKeramba)
+            } else {
+                throw Exception(response.body()?.message)
             }
         }
     }
