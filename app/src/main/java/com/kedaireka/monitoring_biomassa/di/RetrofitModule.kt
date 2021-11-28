33fb10a9d621bffb735bdb16511840165e57b5fc
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-28 13:06:27 +0700

    setup okHttpLogging with retrofit

--- app/src/main/java/com/kedaireka/monitoring_biomassa/di/RetrofitModule.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/di/RetrofitModule.kt
@@ -8,6 +8,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -25,16 +28,42 @@ object RetrofitModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(moshi: Moshi): Retrofit.Builder {
-        return Retrofit.Builder()
-            .baseUrl("https://web-biomassa.000webhostapp.com/api/v1/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    fun providesOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client: OkHttpClient.Builder = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+
+                val request = original.newBuilder()
+//                    .header("User-Agent", "PostmanRuntime/7.28.3")
+//                    .header("Accept", "*/*")
+//                    .header("Accept-Encoding", "gzip, deflate, br")
+//                    .header("Connection", "keep-alive")
+//                    .header("api-key", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IidhZG1pbicnMjEyMzJmMjk3YTU3YTVhNzQzODk0YTBlNGE4MDFmYzMnIn0.nwRSoKhCeArz7xie5hD26SyWjX0qqUZY6_IV3AkXvi0")
+                    .build()
+
+                chain.proceed(request)
+            }
+            .addInterceptor(interceptor)
+
+        return client.build()
     }
 
     @Singleton
     @Provides
-    fun provideMonitoringService(retrofit: Retrofit.Builder): MonitoringService{
+    fun provideRetrofit(moshi: Moshi, client: OkHttpClient): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl("https://web-biomassa.000webhostapp.com/api/v1/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .client(client)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideMonitoringService(retrofit: Retrofit.Builder): MonitoringService {
         return retrofit
             .build()
             .create(MonitoringService::class.java)
