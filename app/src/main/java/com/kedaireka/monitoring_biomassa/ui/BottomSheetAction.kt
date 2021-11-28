6dcb96a3715d6b8d4380ab358fe782f4c2b9dc46
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-11-28 20:58:27 +0700

    setup BottomSheetAction

--- app/src/main/java/com/kedaireka/monitoring_biomassa/ui/BottomSheetAction.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/ui/BottomSheetAction.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.kedaireka.monitoring_biomassa.databinding.BottomSheetActionBinding
+import com.kedaireka.monitoring_biomassa.ui.add.BottomSheetKeramba
 
 class BottomSheetAction: BottomSheetDialogFragment() {
     private lateinit var binding: BottomSheetActionBinding
@@ -17,6 +18,32 @@ class BottomSheetAction: BottomSheetDialogFragment() {
     ): View? {
         binding = BottomSheetActionBinding.inflate(inflater)
 
+        binding.bottomSheetAction = this@BottomSheetAction
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun showBottomSheetEditKeramba(){
+        if (childFragmentManager.findFragmentByTag("BottomSheetKeramba") == null) {
+            if (this.arguments != null) {
+                val bottomSheetKeramba = BottomSheetKeramba()
+
+                val bundle = Bundle()
+
+                val kerambaId = arguments!!.getInt("keramba_id")
+
+                bundle.putInt("keramba_id", kerambaId)
+
+                bottomSheetKeramba.arguments = bundle
+
+                bottomSheetKeramba.show(childFragmentManager, "BottomSheetKeramba")
+            }
+        }
     }
 }
