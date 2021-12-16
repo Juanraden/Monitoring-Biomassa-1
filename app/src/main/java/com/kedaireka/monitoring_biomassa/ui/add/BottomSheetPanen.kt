d1e5e013299655806300382c1cf63ee3cee12bcd
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-12-16 15:03:34 +0700

    panen with online repo

--- app/src/main/java/com/kedaireka/monitoring_biomassa/ui/add/BottomSheetPanen.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/ui/add/BottomSheetPanen.kt
@@ -4,33 +4,38 @@ import android.app.DatePickerDialog
 import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.kedaireka.monitoring_biomassa.R
 import com.kedaireka.monitoring_biomassa.data.domain.BiotaDomain
 import com.kedaireka.monitoring_biomassa.data.domain.KerambaDomain
+import com.kedaireka.monitoring_biomassa.data.network.enums.NetworkResult
 import com.kedaireka.monitoring_biomassa.databinding.BottomSheetPanenBinding
 import com.kedaireka.monitoring_biomassa.ui.DatePickerFragment
 import com.kedaireka.monitoring_biomassa.util.convertLongToDateString
+import com.kedaireka.monitoring_biomassa.viewmodel.BiotaViewModel
 import com.kedaireka.monitoring_biomassa.viewmodel.KerambaViewModel
 import com.kedaireka.monitoring_biomassa.viewmodel.PanenViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
 @AndroidEntryPoint
-class BottomSheetPanen: BottomSheetDialogFragment(), AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener {
+class BottomSheetPanen : BottomSheetDialogFragment(), AdapterView.OnItemSelectedListener,
+    DatePickerDialog.OnDateSetListener {
     private lateinit var binding: BottomSheetPanenBinding
 
-    private val panenViewModel by viewModels<PanenViewModel>()
+    private val panenViewModel by activityViewModels<PanenViewModel>()
 
     private val kerambaViewModel by activityViewModels<KerambaViewModel>()
+
+    private val biotaViewModel by activityViewModels<BiotaViewModel>()
 
     private lateinit var mapKerambatoBiota: Map<KerambaDomain, List<BiotaDomain>>
 
@@ -49,6 +54,30 @@ class BottomSheetPanen: BottomSheetDialogFragment(), AdapterView.OnItemSelectedL
 
         behavior.peekHeight = 3000
         behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+        })
     }
 
     override fun onCancel(dialog: DialogInterface) {
@@ -87,6 +116,75 @@ class BottomSheetPanen: BottomSheetDialogFragment(), AdapterView.OnItemSelectedL
         binding.ivClose.setOnClickListener { dismiss() }
 
         setupDropdown()
+
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        panenViewModel.requestPostAddResult.observe(viewLifecycleOwner, { result ->
+            when (result) {
+                is NetworkResult.Loading -> {
+                }
+                is NetworkResult.Loaded -> {
+                    if (panenViewModel.inputKerambaId.value != null) {
+                        panenViewModel.fetchPanen(panenViewModel.inputKerambaId.value!!)
+
+                        biotaViewModel.fetchBiota(panenViewModel.inputKerambaId.value!!)
+                    }
+
+                    panenViewModel.donePostAddRequest()
+
+                    this.dismiss()
+                }
+                is NetworkResult.Error -> {
+                    if (result.message != "") {
+                        Toast.makeText(requireContext(), result.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        })
+    }
+
+    fun savePanen() {
+        binding.apply {
+            if (isEntryValid(
+                    binding.panjangBiotaEt.text.toString(),
+                    binding.bobotBiotaEt.text.toString(),
+                    binding.jumlahBiotaEt.text.toString(),
+                    binding.jumlahGagalBiotaEt.text.toString(),
+                    binding.tanggalPanenEt.text.toString()
+                )
+            ) {
+                panenViewModel.insertPanen(
+                    binding.panjangBiotaEt.text.toString(),
+                    binding.bobotBiotaEt.text.toString(),
+                    binding.jumlahBiotaEt.text.toString(),
+                    binding.jumlahGagalBiotaEt.text.toString(),
+                    binding.tanggalPanenEt.text.toString()
+                )
+            } else {
+                if (TextUtils.isEmpty(panjangBiotaEt.text)) {
+                    panjangBiotaEt.error = "Panjang biota harus diisi!"
+                }
+
+                if (TextUtils.isEmpty(bobotBiotaEt.text)) {
+                    bobotBiotaEt.error = "Bobot biota harus diisi!"
+                }
+
+                if (TextUtils.isEmpty(jumlahBiotaEt.text)) {
+                    jumlahBiotaEt.error = "Jumlah biota harus diisi!"
+                }
+
+                if (TextUtils.isEmpty(jumlahGagalBiotaEt.text)) {
+                    jumlahGagalBiotaEt.error = "Jumlah gagal panen harus diisi!"
+                }
+
+                if (TextUtils.isEmpty(tanggalPanenEt.text)) {
+                    tanggalPanenEt.error = "Tanggal panen harus diisi!"
+                }
+            }
+        }
     }
 
     private fun setupDropdown() {
@@ -108,10 +206,10 @@ class BottomSheetPanen: BottomSheetDialogFragment(), AdapterView.OnItemSelectedL
 
             binding.kerambaDropdown.adapter = kerambaAdapter
 
-            if (this@BottomSheetPanen.arguments != null){
+            if (this@BottomSheetPanen.arguments != null) {
                 val kerambaId = this@BottomSheetPanen.arguments!!.getInt("keramba_id")
 
-                if (kerambaId > 0){
+                if (kerambaId > 0) {
                     val index = kerambaIdlist.indexOf(kerambaId)
 
                     binding.kerambaDropdown.setSelection(index)
@@ -128,8 +226,14 @@ class BottomSheetPanen: BottomSheetDialogFragment(), AdapterView.OnItemSelectedL
         }
     }
 
-    private fun isEntryValid(panjang: String, bobot: String, jumlahHidup: String, jumlahMati: String, tanggal: String){
-
+    private fun isEntryValid(
+        panjang: String,
+        bobot: String,
+        jumlahHidup: String,
+        jumlahMati: String,
+        tanggal: String
+    ): Boolean {
+        return panenViewModel.isEntryValid(panjang, bobot, jumlahHidup, jumlahMati, tanggal)
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
@@ -144,7 +248,7 @@ class BottomSheetPanen: BottomSheetDialogFragment(), AdapterView.OnItemSelectedL
 
 
     override fun onItemSelected(parent: AdapterView<*>, p1: View, pos: Int, p3: Long) {
-        when(parent.id){
+        when (parent.id) {
             binding.kerambaDropdown.id -> {
                 val keramba = kerambaList[pos]
 
@@ -172,7 +276,6 @@ class BottomSheetPanen: BottomSheetDialogFragment(), AdapterView.OnItemSelectedL
             }
 
         }
-
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {}
--- app/src/main/java/com/kedaireka/monitoring_biomassa/viewmodel/PanenViewModel.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/viewmodel/PanenViewModel.kt
@@ -1,16 +1,99 @@
 package com.kedaireka.monitoring_biomassa.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.*
+import com.kedaireka.monitoring_biomassa.data.domain.PanenDomain
+import com.kedaireka.monitoring_biomassa.data.network.enums.NetworkResult
 import com.kedaireka.monitoring_biomassa.database.dao.PanenDAO
 import com.kedaireka.monitoring_biomassa.database.relation.PanenAndBiota
+import com.kedaireka.monitoring_biomassa.repository.PanenRepository
+import com.kedaireka.monitoring_biomassa.util.convertStringToDateLong
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PanenViewModel @Inject constructor(
-    private val panenDAO: PanenDAO
-): ViewModel(){
-    fun getlistPanen(): LiveData<List<PanenAndBiota>> = panenDAO.getAllPanenAndBiota().asLiveData()
+    private val panenDAO: PanenDAO,
+    private val repository: PanenRepository
+) : ViewModel() {
+    private val _requestGetResult = MutableLiveData<NetworkResult>()
+    val requestGetResult: LiveData<NetworkResult> = _requestGetResult
+
+    private val _requestPostAddResult = MutableLiveData<NetworkResult>()
+    val requestPostAddResult: LiveData<NetworkResult> = _requestPostAddResult
+
+    fun doneToastException() {
+        _requestGetResult.value = NetworkResult.Error("")
+    }
+
+    fun donePostAddRequest() {
+        _requestPostAddResult.value = NetworkResult.Loading()
+    }
+
+    fun getlistPanen(kerambaId: Int): LiveData<List<PanenAndBiota>> =
+        panenDAO.getAllPanenAndBiota(kerambaId).asLiveData()
+
+    private val _inputKerambaId = MutableLiveData<Int>()
+    val inputKerambaId: LiveData<Int> = _inputKerambaId
+
+    private val _inputBiotaId = MutableLiveData<Int>()
+    val inputBiotaId: LiveData<Int> = _inputBiotaId
+
+    fun selectKeramba(id: Int) {
+        _inputKerambaId.value = id
+    }
+
+    fun selectBiota(id: Int) {
+        _inputBiotaId.value = id
+    }
+
+    fun fetchPanen(kerambaId: Int) {
+        _requestGetResult.value = NetworkResult.Loading()
+
+        viewModelScope.launch {
+            try {
+                repository.refreshPanen(kerambaId)
+
+                _requestGetResult.value = NetworkResult.Loaded()
+            } catch (e: Exception) {
+                _requestGetResult.value = NetworkResult.Error(e.message.toString())
+            }
+        }
+    }
+
+    fun insertPanen(
+        panjang: String,
+        bobot: String,
+        jumlahHidup: String,
+        jumlahMati: String,
+        tanggal: String
+    ) {
+        _requestPostAddResult.value = NetworkResult.Loading()
+
+        viewModelScope.launch {
+            try {
+                repository.addPanen(
+                    PanenDomain(
+                        activity_id = 0,
+                        keramba_id = _inputKerambaId.value!!,
+                        biota_id = _inputBiotaId.value!!,
+                        panjang = panjang.toDouble(),
+                        bobot = bobot.toDouble(),
+                        jumlah_hidup = jumlahHidup.toInt(),
+                        jumlah_mati = jumlahMati.toInt(),
+                        tanggal_panen = convertStringToDateLong(tanggal, "EEEE dd-MMM-yyyy")
+                    )
+                )
+                _requestPostAddResult.value = NetworkResult.Loaded()
+            } catch (e: Exception) {
+                _requestPostAddResult.value = NetworkResult.Error(e.message.toString())
+            }
+        }
+    }
+
+    fun isEntryValid(
+        panjang: String, bobot: String, jumlahHidup: String, jumlahMati: String, tanggal: String
+    ): Boolean {
+        return !(_inputBiotaId.value == null || _inputKerambaId.value == null || panjang.isBlank() || bobot.isBlank() || jumlahHidup.isBlank() || jumlahMati.isBlank() || tanggal.isBlank())
+    }
 }
