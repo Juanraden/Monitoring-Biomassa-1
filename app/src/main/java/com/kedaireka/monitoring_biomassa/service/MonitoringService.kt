166df05c01c9e80109131f33681a3c711c0718bf
LAPTOP-M8OJ4LN0\Juanraden <juanraden@gmail.com>
2021-12-23 03:27:53 +0700

    update MonitoringService feeding

--- app/src/main/java/com/kedaireka/monitoring_biomassa/service/MonitoringService.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/service/MonitoringService.kt
@@ -2,7 +2,6 @@ package com.kedaireka.monitoring_biomassa.service
 
 import com.kedaireka.monitoring_biomassa.data.network.container.*
 import kotlinx.coroutines.Deferred
-import okhttp3.internal.http.hasBody
 import retrofit2.Response
 import retrofit2.http.*
 
@@ -151,16 +150,16 @@ interface MonitoringService {
     ): Deferred<Response<FeedingContainer>>
 
     @FormUrlEncoded
-    @POST("feeding/update")
+    @PUT("feeding")
     fun updateFeedingAsync(
-        @Header("api-key") token: String,
+        @Header("Authorization") token: String,
         @FieldMap data: Map<String, String>
     ): Deferred<Response<FeedingContainer>>
 
     @FormUrlEncoded
-    @POST("feeding/delete")
+    @HTTP(method = "DELETE", path = "feeding", hasBody = true)
     fun deleteFeedingAsync(
-        @Header("api-key") token: String,
+        @Header("Authorization") token: String,
         @FieldMap data: Map<String, String>
     ): Deferred<Response<FeedingContainer>>
 
