824a5975032bbd05be026671ce27f92cd7d92f89
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-12-25 10:29:37 +0700

    change login error to string instead of stringres int

--- app/src/main/java/com/kedaireka/monitoring_biomassa/data/network/LoginResult.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/data/network/LoginResult.kt
@@ -5,5 +5,5 @@ package com.kedaireka.monitoring_biomassa.data.network
  */
 data class LoginResult(
     val success: LoggedInUserView? = null,
-    val error: Int? = null
+    val error: String? = null
 )
