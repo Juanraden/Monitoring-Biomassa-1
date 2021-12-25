4a5e45a37caf05a04b6c2a4fe983ae0ebe4b3cd1
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-12-25 21:22:53 +0700

    added time picker fragment

---
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/ui/TimePickerFragment.kt
@@ -0,0 +1,22 @@
+package com.kedaireka.monitoring_biomassa.ui
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class TimePickerFragment : DialogFragment(){
+
+    companion object {
+        fun create(): TimePickerFragment = TimePickerFragment()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        return TimePickerDialog(requireContext(), parentFragment as TimePickerDialog.OnTimeSetListener, hour, minute, false)
+    }
+}
