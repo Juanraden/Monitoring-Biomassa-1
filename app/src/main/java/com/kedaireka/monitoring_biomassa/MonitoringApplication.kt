134bd84d72e714f6e70bb8a3c9aaec63b13606a5
LAPTOP-M8OJ4LN0\Juanraden <juanraden@gmail.com>
2022-03-02 22:13:49 +0700

    Add night mode switch

--- app/src/main/java/com/kedaireka/monitoring_biomassa/MonitoringApplication.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/MonitoringApplication.kt
@@ -1,7 +1,21 @@
 package com.kedaireka.monitoring_biomassa
 
 import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MonitoringApplication: Application()
+class MonitoringApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val sharedPreferences = getSharedPreferences(applicationContext.packageName, Context.MODE_PRIVATE)
+        val isNightMode: Boolean = sharedPreferences?.getBoolean("NIGHT_MODE", false) ?: false
+        if (isNightMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+}
--- app/src/main/java/com/kedaireka/monitoring_biomassa/ui/SettingsFragment.kt
+++ app/src/main/java/com/kedaireka/monitoring_biomassa/ui/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.kedaireka.monitoring_biomassa.ui
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -58,6 +60,8 @@ class SettingsFragment : Fragment() {
         }
 
         setupNavigation()
+
+        setupNightModeSwitch()
     }
 
     fun launchUri(view: View) {
@@ -102,6 +106,25 @@ class SettingsFragment : Fragment() {
 
         binding.toolbarFragment.setNavigationOnClickListener {
             navController.navigateUp(appBarConfiguration)
+        }
+    }
+
+    private fun setupNightModeSwitch() {
+        val sharedPreferences = activity?.getSharedPreferences(activity?.packageName, Context.MODE_PRIVATE)
+
+        val isNightMode: Boolean = sharedPreferences?.getBoolean("NIGHT_MODE", false) ?: false
+        if (isNightMode) {
+            binding.modeGelapSwitch.isChecked = true
+        }
+
+        binding.modeGelapSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                sharedPreferences?.edit()?.putBoolean("NIGHT_MODE", true)?.apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                sharedPreferences?.edit()?.putBoolean("NIGHT_MODE", false)?.apply()
+            }
         }
     }
 }
